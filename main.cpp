61665072307a93e12f4f1fe2fb590b5766c82fe2
chantzuyi <F04066028@gs.ncku.edu.tw>
2021-10-19 23:50:36 +0800

    feat: implement maze escape algorithm

--- main.cpp
+++ main.cpp
@@ -15,8 +15,8 @@
 using namespace std;
 
 #define SIZE 17
-vector<vector<int> > create_maze(string filename);
-void print(vector<vector<int> > array);
+vector<vector<bool> > create_maze(string filename);
+void print(vector<vector<bool> > array);
 
 struct position
 {
@@ -31,34 +31,81 @@ struct offsets
 int main()
 {
     string filename;
-    vector<vector<int> > maze;
-    vector<vector<int> > visited;
-    const vector<offsets> move;
-    enum directions {E, S, N, W};
+    vector<vector<bool> > maze;
+    vector<vector<bool> > visited(SIZE, vector<bool> (SIZE, false));
     stack<position> path;
+    enum directions {E, S, N, W};
+    const vector<offsets> move = {{0, 1}, {1, 0}, {-1, 0}, {0, -1}};  // E, S, N, W
 
     cout << "enter filename : ";
     cin >> filename;
     maze = create_maze(filename);
     print(maze);
 
-    int x_start, y_start, x_end, y_end;
+    int x_start, y_start, x_exit, y_exit;
     while (true)
     {
+        // user input
         cout << "enter start position : ";
         cin >> x_start >> y_start;
         if ((x_start == -1) && (y_start == -1)) { break; }
         cout << "enter exit position : ";
-        cin >> x_end >> y_end;
-        if ((x_end == -1) && (y_end == -1)) { break; }
+        cin >> x_exit >> y_exit;
+        if ((x_exit == -1) && (y_exit == -1)) { break; }
+
+        // clear data structures
+        for (auto& sub : visited) {
+            fill(sub.begin(), sub.end(), 0);
+        }
+        while (!path.empty())
+        {
+            path.pop();
+        }
+        // initialize starting point
+        position current = {x_start, y_start, E};
+        path.push(current);
+        visited[x_start][y_start] = true;
+
+        // algorithm
+        while (!path.empty())
+        {
+            current = path.top();
+            path.pop();
+            while (current.dir <= W)
+            {
+                int x_goto = current.x + move[current.dir].x;
+                int y_goto = current.y + move[current.dir].y;
+
+                if ((x_goto == x_exit) && (y_goto == y_exit))
+                {
+                    cout << "Successfully escaped!!" << endl;
+                    goto endProblem;
+                }
+
+                if ((!maze[x_goto][y_goto]) && (!visited[x_goto][y_goto]))
+                {
+                    current.dir += 1;
+                    path.push(current);
+                    current.x = x_goto;
+                    current.y = y_goto;
+                    current.dir = E;
+                    visited[x_goto][y_goto] = true;
+                }
+                else current.dir++;
+            }
+        }
+        cout << "Failed to escape." << endl;
+
+        endProblem: ;
     }
+    cout << "End of code.";
 
     return 0;
 }
 
-vector<vector<int> > create_maze(string filename)
+vector<vector<bool> > create_maze(string filename)
 {
-    vector<vector<int> > maze(SIZE);
+    vector<vector<bool> > maze(SIZE);
     string line;
     int row = 0;
 
@@ -80,7 +127,7 @@ vector<vector<int> > create_maze(string filename)
     return maze;
 }
 
-void print(vector<vector<int> > array)
+void print(vector<vector<bool> > array)
 {
     for (int i = 0; i < SIZE; ++i)
     {
