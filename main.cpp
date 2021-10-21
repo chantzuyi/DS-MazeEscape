5a66fc55c6df1497754024ff0677f6d646180aa7
tzuyichan <F04066028@gs.ncku.edu.tw>
2021-10-21 18:08:50 +0800

    docs: replace the concept "possible step" with "unvisited tile"
    If a maze tile with coordinates (x, y) is not a wall ( tile(x, y) = 0 )
    and has not been visited previously by the rat, it is called a "possible
     step". However, a "step" implies an action instead of a position, so
    "unvisited tile" is a more suitable name for the concept.

--- main.cpp
+++ main.cpp
@@ -17,9 +17,9 @@ using namespace std;
 
 const int SIZE = 17;
 
-struct position
+struct position  // position format for each tile
 {
-    int x, y, dir;
+    int x, y, dir;  // row, column, direction (enum directions)
 };
 
 struct offsets
@@ -40,19 +40,24 @@ int main()
     const vector<offsets> move = {{0, 1}, {1, 0}, {-1, 0}, {0, -1}};  // position change
                                                                       // in the four directions
                                                                       // E, S, N, W
-
-    // User input name of file that contains the maze problem.
-    // The maze would be converted to a 2D vector then printed to console.
+    /*
+     * User input name of file that contains the maze problem.
+     * The maze would be converted to a 2D vector then printed to console.
+     */
     cout << "enter filename : ";
     cin >> filename;
     maze = create_maze(filename);
     print(maze);
 
-    // Enter program flow structure.
-    // Exit when user input = (-1, -1).
+    /*
+     * Enter program flow structure.
+     * Exit when user input = (-1, -1).
+     */
     while (true)
     {
-        // User input starting position of the rat and exit position in the maze.
+        /*
+         * User input starting position of the rat and exit position in the maze.
+         */
         int x_start, y_start, x_exit, y_exit;
         cout << "enter start position : ";
         cin >> x_start >> y_start;
@@ -76,11 +81,13 @@ int main()
         path.push(current);
         visited[x_start][y_start] = true;
 
-        // Maze escape algorithm.
-        // Will continually search for a possible step torwards the exit until:
-        // a) the exit is reached
-        // b) all possible steps are exhausted,
-        //    i.e. the rat has backtracked to its starting position.
+        /*
+         * Maze escape algorithm.
+         * Will continually search for an unvisited tile torwards the exit until:
+         * a) the exit is reached
+         * b) all tiles were visited,
+         *    i.e. the rat has backtracked to its starting position.
+         */
         while (!path.empty()) // while there is still room to backtrack
         {
             // Backtrack one step if no advances are possible. (except for the 1st step)
@@ -88,15 +95,15 @@ int main()
             path.pop();
             cout << step_count++ << ":" << current.x << "," << current.y << endl;
 
-            // Try step directions in the order of E, S, N, W.
-            // Will continue to advance if a possible step is found.
-            while (current.dir <= W)  // while not all directions are explored
+            // Explore tiles in the order of direction: E, S, N, W.
+            // Will continuously loop forward if an unvisited tile was found.
+            while (current.dir <= W)
             {
-                // Explore the next step in the direction.
+                // Explore the next tile in the direction.
                 int x_goto = current.x + move[current.dir].x;
                 int y_goto = current.y + move[current.dir].y;
 
-                // The next step is the exit.
+                // The next tile is the exit.
                 if ((x_goto == x_exit) && (y_goto == y_exit))
                 {
                     cout << step_count << ":" << x_goto << "," << y_goto << endl;
@@ -104,13 +111,13 @@ int main()
                     goto endProblem;
                 }
 
-                // The next step has not been visited, i.e. it is a possible step.
+                // The next tile has not been visited, move to that tile.
                 if ((!maze[x_goto][y_goto]) && (!visited[x_goto][y_goto]))
                 {
                     current.dir += 1;  // record next direction to explore should the rat backtrack
                     path.push(current);  // write current position to path
 
-                    // Move rat to the next step.
+                    // Move rat to the next tile.
                     current.x = x_goto;
                     current.y = y_goto;
                     current.dir = E;
@@ -118,13 +125,13 @@ int main()
                     cout << step_count++ << ":" << current.x << "," << current.y << endl;
                 }
 
-                // The next step has been visited or is a wall.
-                // Move on to the next direction.
-                else current.dir++;
+                // The next tile has been visited or is a wall.
+                else current.dir++;  // turn to the next direction
             }
         }
-        cout << "Failed to escape." << endl;
 
+        // All tiles were visited but could not reach the exit.
+        cout << "Failed to escape." << endl;
         endProblem: ;
     }
     cout << "end the code.";
@@ -132,9 +139,11 @@ int main()
     return 0;
 }
 
-// Read maze text from file and convert it to a 2D vector.
-// The vector dimensions is SIZE * SIZE, with false(0) being walkable tiles
-// and true(1) representing walls.
+/*
+ * Read maze text from file and convert it to a 2D vector.
+ * The vector dimensions is SIZE * SIZE, with false(0) being walkable tiles
+ * and true(1) representing walls.
+ */
 vector<vector<bool> > create_maze(string filename)
 {
     vector<vector<bool> > maze(SIZE);
@@ -162,7 +171,9 @@ vector<vector<bool> > create_maze(string filename)
     return maze;
 }
 
-// Print 2D vector to console.
+/*
+ * Print 2D vector to console.
+ */
 void print(const vector<vector<bool> > array)
 {
     for (auto row : array)
